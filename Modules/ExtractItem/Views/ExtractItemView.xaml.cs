9c048c482d9c7823f28a81bc91f5284ab0ae5b0a
YanHan <i@yanhan.dev>
2022-03-16 21:36:43 +0800

    textbox drop

--- Modules/ExtractItem/Views/ExtractItemView.xaml.cs
+++ Modules/ExtractItem/Views/ExtractItemView.xaml.cs
@@ -24,5 +24,21 @@ namespace ExtractItem.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+            object text = e.Data.GetData(DataFormats.FileDrop);
+            TextBox tb = sender as TextBox;
+            if (tb != null)
+            {
+                tb.Text = string.Format("{0}", ((string[])text)[0]);
+            }
+        }
+
+        private void TextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
     }
 }
