4f08a8b27d96646aa32208ff8c681285c30f31a6
YanHan <i@yanhan.dev>
2022-05-18 22:02:19 +0800

    revert to dotnet5

--- test/Program.cs
+++ test/Program.cs
@@ -6,7 +6,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("dotnet5");
             string font = "<font name=\"00008130.Program.Fontset_ItemGrade_8\">洪门宝石结晶</font>";
             Console.WriteLine("Hello World!");
         }
