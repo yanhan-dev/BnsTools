bdab540fe9f2b5bb45f81b61653cb68d420bfb44
Pig <i@yanhan.dev>
2023-05-10 17:56:53 +0800

    add shared xmleditor

---
+++ Modules/XmlEditor_Shared/ViewModels/FilesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Mvvm;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlEditor.ViewModels
+{
+    public class FilesViewModel : BindableBase
+    {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { SetProperty(ref _Name, value); }
+        }
+
+        private string _Uri;
+        public string Uri
+        {
+            get { return _Uri; }
+            set { SetProperty(ref _Uri, value); }
+        }
+
+        private string _Desc;
+        public string Desc
+        {
+            get { return _Desc; }
+            set { SetProperty(ref _Desc, value); }
+        }
+    }
+}
